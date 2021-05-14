807c67a9f3875abeb203c51e23c3707cb9c290fe
Florian Hofer <info@florianhofer.it>
2021-05-14 13:11:35 +0200

    Clean startTest.h to act as runner for Python

--- startTest.sh
+++ startTest.sh
@@ -21,63 +21,10 @@ fi
 
 ############################### global variables ####################################
 
-#Initialization
-baudRate=9600
-groups=('A' 'B' 'C' 'D')
-tests=(4 3 5 0)
-testNo=
-testGrp=
-resprnt=0
-ports=( )
-types=( )
+PYTHON3="python3"
+
 
 ############################### device comm functions ####################################
-
-#device paramteters
-
-function waitFree() {
-    port=${1:-'/dev/ttyUSB0'}
-
-    while read line;
-    do
-        echo "$(date): $line" 
-    done
-}
-
-function readMicro() {
-    port=${1:-'/dev/ttyUSB0'}
-
-    while read line;
-    do
-        if [ "$line" == "Results" ]; then
-            resprnt=1
-        fi
-        if [ "$line" == "done" ]; then
-            resprnt=0
-        fi
-
-        echo "$(date): $line" 
-        if [ $resprnt -eq 1 ]
-        then
-            echo >> testResults$testGrp$testNo.csv
-        fi
-    done
-    return $resprnt
-}
-
-function getMicroType() {
-	#echo "T\n" > $1
-	read -p "T\n" -t 1 resp > $1
-	if [[ "$resp" == "MKRWAN"* ]]; 
-	then
-		return 1	
-	elif [[ "$resp" == "TTNUNO"* ]]; 
-	then
-		return 2
-	else
-		return 0
-	fi
-}
 
 
 function setupSerial() {
@@ -87,17 +34,11 @@ function setupSerial() {
 
         for port in /dev/ttyUSB* ; do
             [ -e "$port" ] || continue
-            ports+=($port)
             stty $baudRate -F $port raw -echo
-            types+=getMicroType $port
-            waitFree $port
         done
         for port in /dev/ttyACM* ; do
             [ -e "$port" ] || continue
-            ports+=($port)
             stty $baudRate -F $port raw -echo
-            waitFree $port
-            types+=getMicroType $port
         done
 
     elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -105,15 +46,11 @@ function setupSerial() {
 
         for port in /dev/cu.usbmodem* ; do
             [ -e "$port" ] || continue
-            ports+=($port)
             stty -f $port $baudRate raw -echo
-            waitFree $port
-            types+=getMicroType $port            
         done
 
     fi
 }
-
 
 ############################### Functions, generic ####################################
 
@@ -138,6 +75,14 @@ EOF
 	exit 1	
 }
 
+function checkLibraries(){
+	eval "pip list | grep -F pyusb > /dev/null"
+	if [ "$?" -eq 0 ] ; then
+		return 0
+	else
+	    return 1
+	fi
+}
 
 function createRamDisk() {
     echo "Creating RamDisk for logs"
@@ -145,110 +90,28 @@ function createRamDisk() {
     mount -t tmpfs -o size=128m tmpfs /mnt/ramlog
 }
 
-function getNextTestNos(){
-    for i in ${!groups[@]}; do
-        if [ $testGrp = ${groups[$i]} ]; then
-            tno=($(seq  1 ${tests[$i]} ))
-            break
-        fi
-    done
-}
-
-function getNextTest(){
-
-    if [ -z $testGrp ]; then
-        if [ $tgrp = "ALL" ]; then
-            tgrp=(${groups[@]})
-        else
-            eval IFS=',' read -r -a tgrp <<< $tgrp
-        fi
-        testGrp=${tgrp[0]}
-        if [ $tno = "ALL" ]; then 
-            getNextTestNos
-        else
-            eval IFS=',' read -r -a tno <<< $tno
-        fi
-    else
-        for i in ${!groups[@]}; do
-            if [ $testGrp = ${groups[$i]} ]; then
-                if [ $testNo -ge ${tests[$i]} ]; then
-                    # goto next test, pop 
-                    unset tgrp[0]
-                    tgrp=( ${tgrp[@]} )
-                    testGrp=${tgrp[0]}
-                    if [ -z $tgrp ]; then
-                        echo $tgrp
-                        return 1
-                    fi
-                    getNextTestNos
-                else
-                    unset tno[0]
-                    tno=( ${tno[@]} )
-                    if [ -z $tno ]; then
-                        echo $tno
-                        return 1
-                    fi
-                fi
-                break
-            fi
-        done
-    fi
-    # save to next test variables
-    testGrp=${tgrp[0]}
-    testNo=${tno[0]}
-    return 0
-}
-
-############################### cmd specific func ####################################
-
-function runTest() {
-    port=${1:-'/dev/ttyUSB0'}
-    grp=${2:-'A'}
-    test=${3:-1}
-
-    waitFree $port
-
-    echo g${grp}t${test}r > $port
-}
-
-############################### cmd specific exec ####################################
-
-# if [ $# -lt 1 ]; then
-# 	echo "Not enough arguments supplied!"
-# 	printUsage
-# fi
+#if [ $# -lt 1 ]; then
+#	echo "Not enough arguments supplied!"
+#	printUsage
+#fi
 
 cmd=${1:-'test'}
 shift
 
 if [[ $cmd == "test" ]]; then
-    tgrp=${1:-'A'}
-    shift
-    tno=${1:-'ALL'}
-    shift
 
-    setupSerial
+	checkLibraries
+	if [ "$?" -ne 0 ]; then
+		echo "Required Python libraries not present and/or installable."
+		echo "Refer to README for details"
+		exit 1
+	fi
 	
-    echo ${ports[@]}
-    while getNextTest 
-    do
-        echo Test $testGrp $testNo
-        for port in ports
-        do
-            runTest port
-        done
-
-        for port in ports
-        do
-            waitDone $port
-        done
-    done
-
+    setupSerial
+    # createRamDisk
+	eval $PYTHON3 testMain.py	
+	
 else
     echo "Unknown command!!"
     printUsage
 fi
-
-
-
-
