7c4e45dfba37cf453ba1fc7ee2a1a50e5ea991e1
Florian Hofer <info@florianhofer.it>
2021-05-17 10:24:39 +0200

    Test runs, main

--- src/testMain.py
+++ src/testMain.py
@@ -1,4 +1,7 @@
 import deviceMgmt
+import threading
+import time
+import traceback
 
 """
 Main program
@@ -10,13 +13,21 @@ Read present micros, configure and start the tests as indicated (parameters?)
 deviceMgmt.findArduinos()
 
 # test
+time.sleep(1)
 try:
-    deviceMgmt.microList[0].write("T\n")
+    uin = input()
+    while uin != "":
+        deviceMgmt.microList[0].write( uin +"\n")
+        while True:
+            try:
+                print(deviceMgmt.microList[0].read(1000))
+            except:
+                break;
+        uin = input();
     
-    while True:
-        try:
-            print(deviceMgmt.microList[0].read(100))
-        except:
-            break;
-except:
-    pass
+except Exception as e:
+    traceback.print_exc()
+
+
+# Trigger destructor call
+del deviceMgmt.microList
